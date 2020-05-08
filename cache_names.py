dd6a12e5d88acf0e51ee6a7af541099426bf4263
Sean Breckenridge <seanbrecke@gmail.com>
2020-05-07 21:59:57 -0700

    remove duplicate code
    use a sort-of enum to put 'anime'/'manga'
    into the requests, instead of duplicating the
    for loops

--- cache_names.py
+++ cache_names.py
@@ -8,8 +8,8 @@ and if the entry is NSFW
 import os
 import json
 import time
+from typing import Set
 
-import requests
 import jikanpy
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -17,10 +17,12 @@ jikan = jikanpy.Jikan("http://localhost:8000/v3/")
 
 id_cache_file = os.path.join(base_dir, "unapproved.json")
 info_cache_file = os.path.join(base_dir, "unapproved_info.json")
-
 a_k = "unapproved_anime"
 m_k = "unapproved_manga"
 
+
+# request from jikan, with backoff
+# return the name, type, and whether this is sfw/nsfw
 def request_jikan(endpoint, _id, retry=0, ex=None):
     if retry >= 5:
         print(ex)
@@ -30,40 +32,55 @@ def request_jikan(endpoint, _id, retry=0, ex=None):
         resp = getattr(jikan, endpoint)(_id)
         time.sleep(3)
         nsfw = 12 in [g["mal_id"] for g in resp["genres"]]
-        return {"name": str(resp["title"]), "type": str(resp["type"]), "nsfw": nsfw}
-    except (jikanpy.exceptions.JikanException, jikanpy.exceptions.APIException) as jex:
-        print("...failed with {}, retrying ({} of 5)".format(type(jex).__name__, retry))
+        return {
+            "name": str(resp["title"]),
+            "type": str(resp["type"]),
+            "nsfw": nsfw
+        }
+    except (jikanpy.exceptions.JikanException,
+            jikanpy.exceptions.APIException) as jex:
+        print("...failed with {}, retrying ({} of 5)".format(
+            type(jex).__name__, retry))
         time.sleep((retry + 1) * 2)
         request_jikan(endpoint, _id, retry + 1, jex)
 
 
-
 def main():
+    # grab unapproved ID cache
     with open(id_cache_file) as id_f:
         id_cache = json.load(id_f)
+
+    # load previous run cache
     if os.path.exists(info_cache_file):
         with open(info_cache_file) as c_f:
             name_cache = json.load(c_f)
     else:
+        # default cache if broken
         name_cache = {a_k: {}, m_k: {}}
 
-    for anime_id in id_cache["unapproved_anime"]:
-        id_str = str(anime_id)
-        if id_str not in name_cache[a_k]:
-            response = request_jikan("anime", anime_id)
-            if response is not None:
-                name_cache[a_k][id_str] = response
+    for req_type, json_key, name_cache_key in (
+        ("anime", "unapproved_anime", a_k),
+        ("manga", "unapproved_manga", m_k),
+    ):
+        # cache new information
+        for type_id in id_cache[json_key]:
+            id_str = str(type_id)
+            if id_str not in name_cache[name_cache_key]:
+                response = request_jikan(req_type, type_id)
+                if response is not None:  # is None when request fails 5 times
+                    name_cache[name_cache_key][id_str] = response
 
-    for manga_id in id_cache["unapproved_manga"]:
-        id_str = str(manga_id)
-        if id_str not in name_cache[m_k]:
-            response = request_jikan("manga", manga_id)
-            if response is not None:
-                name_cache[m_k][id_str] = response
+        # remove old information
+        current_ids: Set[str] = set(map(str, id_cache[json_key]))
+        # print(len(name_cache[name_cache_key].keys()))
+        for id_str in list(name_cache[name_cache_key]):
+            if id_str not in current_ids:
+                del name_cache[name_cache_key][id_str]
+        # print(len(name_cache[name_cache_key].keys()))
 
     with open(info_cache_file, 'w') as c_f:
         json.dump(name_cache, c_f)
 
+
 if __name__ == "__main__":
     main()
-
